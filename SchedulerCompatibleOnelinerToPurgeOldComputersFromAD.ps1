eeb0e8d35ad621a6d801208dad6386b721cf0ad5
Darwin <darwinjsgithub.services@sanoys.com>
2018-03-17 11:13:57 -0400

    Updates

--- SchedulerCompatibleOnelinerToPurgeOldComputersFromAD.ps1
+++ SchedulerCompatibleOnelinerToPurgeOldComputersFromAD.ps1
@@ -1,6 +1,6 @@
 
-#See this post for full details on why this code is helpful: PLACE URL
+#See this post for full details on why this code is helpful: https://cloudywindows.io/post/culling-dead-computer-records-from-ad-with-a-scheduled-powershell-oneliner/
 
 # This can be scheduled as SYSTEM on a domain controller - creates a CSV of whatever was removed
 # AFTER RUNNING A TEST REPORT SCHEDULED UNDER SYSTEM ACCOUNT, Change $PurgeThreshold to less than 10 years and set $ReallyDelete $True
-$ReallyDelete = $False ; $PurgeThreshold = -3650 ; $removelist = @(search-adaccount -accountinactive -datetime (get-date).adddays($PurgeThreshold) -computersonly) ; If ($removelist.count -lt 1) {Exit 0} ; $removelist | sort-object LastLogonDate | select-object Name, LastLogonDate, DistinguishedName, SID, ObjectGUID | export-csv -notypeinformation -path "$env:public\AD-ComputerCleanUp-At-$(Get-date -format 'yyyyMMddHHmm').csv" ; If ($ReallyDelete) {$removelist | Remove-ADComputer -confirm:$false }
+$ReallyDelete = $False ; $PurgeThreshold = -3650 ; $RemoveList = @(Search-ADAccount -AccountInactive -DateTime (get-date).AddDays($PurgeThreshold) -ComputersOnly) ; If ($RemoveList.count -lt 1) {Exit 0} ; $RemoveList | Sort-Object LastLogonDate | Select-Object Name, LastLogonDate, DistinguishedName, SID, ObjectGUID | Export-Csv -NoTypeInformation -Path "$env:public\AD-ComputerCleanUp-At-$(Get-date -format 'yyyyMMddHHmm').csv" ; If ($ReallyDelete) {$RemoveList | Remove-ADComputer -Confirm:$False}
